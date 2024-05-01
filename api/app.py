cb1424f6e9ff718a93dcac3f17506dd020d6f0de
jason <zsz417@163.com>
2024-05-01 10:14:46 +0800

    revert

--- api/app.py
+++ api/app.py
@@ -62,10 +62,10 @@ def write_providers_json(data):
     if not temp_json_data:
         with open('providers.json', 'w', encoding='utf-8') as json_file:
             json.dump(data, json_file, indent=4, ensure_ascii=False)
-            
-@app.route('/')
-def index():
-    return redirect('https://www.google.com')
+
+# @app.route('/')
+# def index():
+    # return redirect('https://www.google.com')
 
 @app.route('/1')
 def index():
